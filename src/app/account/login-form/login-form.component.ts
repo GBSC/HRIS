0472d081afdfc4b5e92707e5e0d3fe0d3af48310
Mujtaba <info@gbscinc.com>
2018-05-28 13:13:33 +0500

    updated

--- src/app/account/login-form/login-form.component.ts
+++ src/app/account/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs';
-import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { Credentials } from '../../shared/models/credentials.interface';
@@ -15,25 +15,26 @@ export class LoginFormComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription;
 
+  loading: boolean;
   brandNew: boolean;
   errors: string;
   isRequesting: boolean;
   submitted: boolean = false;
   credentials: Credentials = { email: '', password: '' };
 
-  constructor(private userService: UserService, private router: Router,private activatedRoute: ActivatedRoute) { }
+  constructor(private userService: UserService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
-    ngOnInit() {
+  ngOnInit() {
 
     // subscribe to router event
     this.subscription = this.activatedRoute.queryParams.subscribe(
       (param: any) => {
-         this.brandNew = param['brandNew'];   
-         this.credentials.email = param['email'];         
-      });      
+        this.brandNew = param['brandNew'];
+        this.credentials.email = param['email'];
+      });
   }
 
-   ngOnDestroy() {
+  ngOnDestroy() {
     // prevent memory leak by unsubscribing
     this.subscription.unsubscribe();
   }
@@ -41,14 +42,16 @@ export class LoginFormComponent implements OnInit, OnDestroy {
   login({ value, valid }: { value: Credentials, valid: boolean }) {
     this.submitted = true;
     this.isRequesting = true;
-    this.errors='';
+    this.loading = true;
+    this.errors = '';
     if (valid) {
       this.userService.login(value.email, value.password)
         .finally(() => this.isRequesting = false)
         .subscribe(
-        result => {         
+        result => {
           if (result) {
-             this.router.navigate(['/dashboard/home']);             
+            this.loading = false;
+            this.router.navigate(['/dashboard/home']);
           }
         },
         error => this.errors = error);
